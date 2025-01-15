66c1aa361eabe3081768848a0c5a613167ac4442
Renato Del Gaudio <renato.delgaudio@gmail.com>
2025-01-15 07:57:21 +0100

    api update

--- uptime.sh
+++ uptime.sh
@@ -8,14 +8,16 @@ UPTIME_FILE=status/uptime_status.txt
 
 echo checking Validator status at $TIMESTAMP > $UPTIME_FILE
 
-CURL_CMD='curl -s  https://undexer.namada.coverlet.io/v4/validator?publicKey=5399BC0F27BB74E104A74C4123AEA8CC90FF9A1EC66AA988D17CE11B835CAAAE&uptime'
+#CURL_CMD='curl -s  https://undexer.namada.coverlet.io/v4/validator?publicKey=5399BC0F27BB74E104A74C4123AEA8CC90FF9A1EC66AA988D17CE11B835CAAAE&uptime'
+CURL_CMD='curl -s  https://api.namada.valopers.com/validators/details/tnam1qyvc8n7ufmpan4yy3nemm3td2c6uff4m6s72g3eg'
 
 VALIDATOR_STATUS=`$CURL_CMD`
 
 echo $VALIDATOR_STATUS >> $UPTIME_FILE
 
 
-COUNTED_BLOCKS=$(echo $VALIDATOR_STATUS | jq -r .countedBlocks )
+#COUNTED_BLOCKS=$(echo $VALIDATOR_STATUS | jq -r .countedBlocks )
+COUNTED_BLOCKS=$(echo $VALIDATOR_STATUS | jq -r .uptime )
 
 if [ "$COUNTED_BLOCKS" = "100" ]; then
    echo "OK_ALL_BLOCK_SIGNED" >> $UPTIME_FILE
